2ab9d7cb9eaa4080f96a78302044be60e3983d13
ha0li <hli@fastly.com>
2022-09-19 14:40:02 -0400

    Add stub for mTLS info hostcalls
    Signed-off-by: ha0li <hli@fastly.com>

--- lib/src/wiggle_abi/req_impl.rs
+++ lib/src/wiggle_abi/req_impl.rs
@@ -10,9 +10,10 @@ use {
             fastly_http_req::FastlyHttpReq,
             headers::HttpHeaders,
             types::{
-                BackendConfigOptions, BodyHandle, CacheOverrideTag, ContentEncodings,
-                DynamicBackendConfig, FramingHeadersMode, HttpVersion, MultiValueCursor,
-                MultiValueCursorResult, PendingRequestHandle, RequestHandle, ResponseHandle,
+                BackendConfigOptions, BodyHandle, CacheOverrideTag, ClientCertVerifyResult,
+                ContentEncodings, DynamicBackendConfig, FramingHeadersMode, HttpVersion,
+                MultiValueCursor, MultiValueCursorResult, PendingRequestHandle, RequestHandle,
+                ResponseHandle,
             },
         },
     },
@@ -117,6 +118,23 @@ impl FastlyHttpReq for Session {
         chello_max_len: u32,
         nwritten_out: &GuestPtr<u32>,
     ) -> Result<(), Error> {
+        Err(Error::NotAvailable("Client TLS data"))
+    }
+
+    #[allow(unused_variables)] // FIXME HL 2022-09-19: Remove this directive once implemented.
+    fn downstream_tls_raw_client_certificate<'a>(
+        &mut self,
+        _raw_client_cert_out: &GuestPtr<'a, u8>,
+        _raw_client_cert_max_len: u32,
+        _nwritten_out: &GuestPtr<u32>,
+    ) -> Result<(), Error> {
+        Err(Error::NotAvailable("Client TLS data"))
+    }
+
+    #[allow(unused_variables)] // FIXME HL 2022-09-19: Remove this directive once implemented.
+    fn downstream_tls_client_cert_verify_result(
+        &mut self,
+    ) -> Result<ClientCertVerifyResult, Error> {
         Err(Error::NotAvailable("Client TLS data"))
     }
 
