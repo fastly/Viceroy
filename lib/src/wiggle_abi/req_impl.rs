9d355801448e291db6d2b56a9c9f82681cdce912
byoung <byoung@fastly.com>
2023-04-19 16:23:23 -0600

    First take at implementing PeekableTask

--- lib/src/wiggle_abi/req_impl.rs
+++ lib/src/wiggle_abi/req_impl.rs
@@ -4,8 +4,8 @@ use {
     crate::{
         config::Backend,
         error::Error,
-        session::{AsyncItem, Session, ViceroyRequestMetadata},
-        upstream::{self, PendingRequest},
+        session::{AsyncItem, PeekableTask, Session, ViceroyRequestMetadata},
+        upstream,
         wiggle_abi::{
             fastly_http_req::FastlyHttpReq,
             headers::HttpHeaders,
@@ -534,7 +534,7 @@ impl FastlyHttpReq for Session {
         Ok(self.insert_response(resp))
     }
 
-    fn send_async<'a>(
+    async fn send_async<'a>(
         &mut self,
         req_handle: RequestHandle,
         body_handle: BodyHandle,
@@ -556,13 +556,13 @@ impl FastlyHttpReq for Session {
 
         // asynchronously send the request
         let pending_req =
-            PendingRequest::spawn(upstream::send_request(req, backend, self.tls_config()));
+            PeekableTask::spawn(upstream::send_request(req, backend, self.tls_config()));
 
         // return a handle to the pending request
-        Ok(self.insert_pending_request(pending_req))
+        Ok(self.insert_pending_request(pending_req.await))
     }
 
-    fn send_async_streaming<'a>(
+    async fn send_async_streaming<'a>(
         &mut self,
         req_handle: RequestHandle,
         body_handle: BodyHandle,
@@ -584,34 +584,31 @@ impl FastlyHttpReq for Session {
 
         // asynchronously send the request
         let pending_req =
-            PendingRequest::spawn(upstream::send_request(req, backend, self.tls_config()));
+            PeekableTask::spawn(upstream::send_request(req, backend, self.tls_config()));
 
         // return a handle to the pending request
-        Ok(self.insert_pending_request(pending_req))
+        Ok(self.insert_pending_request(pending_req.await))
     }
 
     // note: The first value in the return tuple represents whether the request is done: 0 when not
     // done, 1 when done.
-    fn pending_req_poll(
+    async fn pending_req_poll(
         &mut self,
         pending_req_handle: PendingRequestHandle,
     ) -> Result<(u32, ResponseHandle, BodyHandle), Error> {
-        let mut pending_req = self.take_pending_request(pending_req_handle)?;
-        pending_req.poll();
+        let handle: PendingRequestHandle = pending_req_handle.into();
 
-        let outcome = match pending_req {
-            PendingRequest::Waiting(_) => {
-                // Make sure to reinsert if pending
-                self.reinsert_pending_request(pending_req_handle, pending_req)?;
-                (0, INVALID_REQUEST_HANDLE.into(), INVALID_BODY_HANDLE.into())
-            }
-            PendingRequest::Complete(resp) => {
-                // the request is done so we will not reinsert it into the map
-                let (resp_handle, resp_body_handle) = self.insert_response(resp?);
+        let outcome = match self.async_item_mut(handle.into())?.is_some() {
+            true => {
+                let resp = self
+                    .take_pending_request(pending_req_handle)?
+                    .recv()
+                    .await?;
+                let (resp_handle, resp_body_handle) = self.insert_response(resp);
                 (1, resp_handle, resp_body_handle)
             }
+            false => (0, INVALID_REQUEST_HANDLE.into(), INVALID_BODY_HANDLE.into()),
         };
-
         Ok(outcome)
     }
 
@@ -619,8 +616,11 @@ impl FastlyHttpReq for Session {
         &mut self,
         pending_req_handle: PendingRequestHandle,
     ) -> Result<(ResponseHandle, BodyHandle), Error> {
-        let pending_req = self.take_pending_request(pending_req_handle)?;
-        Ok(self.insert_response(pending_req.wait().await?))
+        let pending_req = self
+            .take_pending_request(pending_req_handle)?
+            .recv()
+            .await?;
+        Ok(self.insert_response(pending_req))
     }
 
     // First element of return tuple is the "done index"
@@ -650,14 +650,12 @@ impl FastlyHttpReq for Session {
         )?;
 
         let outcome = match item {
-            AsyncItem::PendingReq(res) => match res {
-                PendingRequest::Complete(resp) => match resp {
-                    Ok(resp) => {
-                        let (resp_handle, body_handle) = self.insert_response(resp);
+            AsyncItem::PendingReq(task) => match task {
+                PeekableTask::Complete(res) => match res {
+                    Ok(res) => {
+                        let (resp_handle, body_handle) = self.insert_response(res);
                         (done_index, resp_handle, body_handle)
                     }
-                    // Unfortunately, the ABI provides no means of returning error information
-                    // from completed `select`.
                     Err(_) => (
                         done_index,
                         INVALID_RESPONSE_HANDLE.into(),
