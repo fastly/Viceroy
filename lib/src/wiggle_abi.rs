8fba2c676c61ead2dd28abbc897a2d9e6d247190
Adam Williams <awilliams@fastly.com>
2023-10-06 03:48:48 -0600

    Lower Hostcall error log level to DEBUG (#314)

--- lib/src/wiggle_abi.rs
+++ lib/src/wiggle_abi.rs
@@ -158,7 +158,7 @@ impl UserErrorConversion for Session {
                     event!(Level::DEBUG, "Hostcall yielded an error: {}", err);
                 }
             },
-            _ => event!(Level::ERROR, "Hostcall yielded an error: {}", e),
+            _ => event!(Level::DEBUG, "Hostcall yielded an error: {}", e),
         }
 
         match e {
