c8bd169027f3924c517559e03596d5b30a103ca7
computermouth <computermouth@crunchbangplusplus.org>
2024-06-04 09:41:54 -0600

    fmt

--- lib/src/component/viceroy_env.rs
+++ lib/src/component/viceroy_env.rs
@@ -1,7 +1,4 @@
-use {
-    super::fastly::api::viceroy_env,
-    crate::session::Session,
-};
+use {super::fastly::api::viceroy_env, crate::session::Session};
 
 #[async_trait::async_trait]
 impl viceroy_env::Host for Session {
