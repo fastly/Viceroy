27908e1fcbbe895103e43bd3b43aae7633ed62d5
Trevor Elliott <telliott@fastly.com>
2024-06-11 13:22:31 -0700

    Fix trap handling, and run trap-test with components

--- lib/src/component/types.rs
+++ lib/src/component/types.rs
@@ -1,3 +1,43 @@
-use {super::fastly::api::types, crate::session::Session};
+use {
+    super::fastly::api::types,
+    crate::{
+        error::{self, HandleError},
+        session::Session,
+    },
+    http::header::InvalidHeaderName,
+};
 
-impl types::Host for Session {}
+pub enum TrappableError {
+    Error(types::Error),
+    Trap(anyhow::Error),
+}
+
+impl types::Host for Session {
+    fn convert_error(&mut self, err: TrappableError) -> wasmtime::Result<types::Error> {
+        match err {
+            TrappableError::Error(err) => Ok(err),
+            TrappableError::Trap(err) => Err(err),
+        }
+    }
+}
+
+impl From<HandleError> for TrappableError {
+    fn from(_: HandleError) -> Self {
+        Self::Error(types::Error::BadHandle)
+    }
+}
+
+impl From<InvalidHeaderName> for TrappableError {
+    fn from(_: InvalidHeaderName) -> Self {
+        Self::Error(types::Error::GenericError)
+    }
+}
+
+impl From<error::Error> for TrappableError {
+    fn from(e: error::Error) -> Self {
+        match e {
+            error::Error::FatalError(_) => Self::Trap(anyhow::anyhow!(e.to_string())),
+            _ => Self::Error(e.into()),
+        }
+    }
+}
