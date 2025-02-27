5df13b9522b54b7ac790188380a44bfc4566394a
Charles Eckman <charles.eckman@fastly.com>
2025-02-27 10:22:39 -0500

    Partial implementation of non-transactional cache API
    Implement the non-transactional API with many limitations:
    - No streaming bodies (which also makes the test brittle)
    - No options processing

---
+++ cli/tests/integration/cache.rs
@@ -0,0 +1,16 @@
+use {
+    crate::{
+        common::{Test, TestResult},
+        viceroy_test,
+    },
+    hyper::StatusCode,
+};
+
+viceroy_test!(request_works, |is_component| {
+    let resp = Test::using_fixture("cache.wasm")
+        .adapt_component(is_component)
+        .against_empty()
+        .await?;
+    assert_eq!(resp.status(), StatusCode::OK);
+    Ok(())
+});
