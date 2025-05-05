04d9b0edc74dc4e8fe6b73d13b30ae4f0cc5753e
Charles Eckman <charles.eckman@fastly.com>
2025-05-05 10:51:42 -0400

    Add test for collapsing across vary rules

--- test-fixtures/src/bin/cache_transactional.rs
+++ test-fixtures/src/bin/cache_transactional.rs
@@ -1,4 +1,5 @@
 use fastly::cache::core::*;
+use http::HeaderName;
 use std::io::{Read, Write};
 use std::time::Duration;
 use uuid::Uuid;
@@ -8,6 +9,7 @@ fn main() {
     test_implicit_cancel_of_fetch();
     test_implicit_cancel_of_pending();
     test_explicit_cancel();
+    test_collapse_across_vary();
 }
 
 fn new_key() -> CacheKey {
@@ -98,4 +100,53 @@ fn test_explicit_cancel() {
     let t2 = pending.wait().unwrap();
     assert!(!t2.found().is_some());
     assert!(t2.must_insert_or_update());
+}
+
+fn test_collapse_across_vary() {
+    let key = new_key();
+
+    let header1 = HeaderName::from_static("header1");
+    let header2 = HeaderName::from_static("header2");
+
+    // Prefill with distinct vary rules, with stale responses:
+    let b = insert(key.clone(), Duration::ZERO)
+        .header(&header1, "value1")
+        .vary_by([&header1].into_iter())
+        .execute()
+        .unwrap();
+    b.finish().unwrap();
+
+    let b = insert(key.clone(), Duration::ZERO)
+        .header(&header2, "value2")
+        .vary_by([&header2].into_iter())
+        .execute()
+        .unwrap();
+    b.finish().unwrap();
+
+    // vary: header2 is the most recent value.
+    // If we start a transaction that matches both rules:
+    let txn2 = Transaction::lookup(key.clone())
+        .header(&header2, "value2")
+        .header(&header1, "value1")
+        .execute_async()
+        .unwrap();
+    // And a transaction that matches only the header1 rule:
+    let txn1 = Transaction::lookup(key.clone())
+        .header(&header1, "value1")
+        .execute_async()
+        .unwrap();
+
+    // Are these requests collapsed?
+    //
+    // If we are using the most-recent-received Vary rule as a heuristic for "what do we expect the
+    // next Vary rule to be", then we should *not* collapse them: the most recent vary rule is
+    // `header2`, and these have distinct `header2` values.
+    //
+    // However, if we're saying "collapse based on any vary rule received in the past", they would
+    // not be collapsed.
+    //
+    // We err on the side of "less latency, more requests": both of these should be outstanding,
+    // because according to the most recent vary rule they are distinct.
+    assert!(!txn2.pending().unwrap());
+    assert!(!txn1.pending().unwrap());
 }
