f085529d26175730a2a8c2153834653396035937
Charles Eckman <charles.eckman@fastly.com>
2025-05-28 14:45:49 -0400

    Integration test for malevolent chunking
    This demonstrates the underlying issue in #479. The origin writes
    maliciously-sized chunks - zero-length for the uncompressed body, two
    bytes at a time for the compressed body - and the test fixture asserts
    that the expected data are read.
    
    This passes for the uncompressed body (yay!); without #480, this fails
    for the uncompressed body (i.e. it's a reproducer for #479).

---
+++ test-fixtures/src/bin/expects-hello.rs
@@ -0,0 +1,16 @@
+use fastly::Request;
+use http::{HeaderName, StatusCode, Version};
+
+fn main() {
+    let mut resp = Request::get("https://fastly.com/")
+        // .with_version(Version::HTTP_2)
+        .with_header(HeaderName::from_static("accept-encoding"), "gzip")
+        .with_auto_decompress_gzip(true)
+        .send("ReturnsHello")
+        .expect("can send request");
+    assert_eq!(resp.get_status(), StatusCode::OK);
+    let got = resp.take_body_str();
+    eprintln!("got: {}", got);
+
+    assert_eq!(&got, "hello world", "got: {}", got);
+}
