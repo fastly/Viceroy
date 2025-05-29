c5528b133692f53cb73a9d6272ff0f3a9f38c908
Charles Eckman <charles.eckman@fastly.com>
2025-05-29 16:39:58 -0400

    Ensure body reads only return 0-length at the end (#483)
    Tests and a slightly broader preventative measure for #479.
    
    The Rust [`Read`](https://doc.rust-lang.org/std/io/trait.Read.html) trait uses a return value of `Ok(0)` to indicate a (permanent/temporary) end-of-stream, e.g. end-of-file.
    We use the same convention for the `fastly_http_body::read` hostcall.
    
    The `Body` [trait](https://docs.rs/http-body/latest/http_body/trait.Body.html) is a little more careful, with a separate `is_stream_end` method distinct from the "length of frame" property.
    
    However, this means if our `Body` implementations _ever_ yield a zero-length chunk, the `fastly_http_body::read` hostcall will look like an end-of-stream to the guest. That's at the root of #479.
    
    #480 addresses the observed cause- that the gzip decoder in particular can yield zero-length chunks when the header + body are split. However, it's in principle possible for another (current or future) variant of `Body` to reintroduce the issue.
    
    -   Test for zero-length bodies, as an integraton test and as a property test. If #480 is patched out, both of these groups of tests reproduce the issue with the gzip body.
    -   Tackle the issue where it occurs, at the conversion from the `http::Body` stream-style API to the `std::io::Read`-style API. Add a `Body::read` method that acts more like the `AsyncRead` traits; use it from the hostcalls, instead of putting `Body` internals in those hostcalls.
    
    Fixed #479 (to my satisfaction)

--- lib/src/body.rs
+++ lib/src/body.rs
@@ -116,6 +116,35 @@ impl Body {
         self.extend(body)
     }
 
+    /// Analogue to the AsyncRead traits, but without running through std::io::Result.
+    ///
+    /// Attempts to read at least one byte into the buffer.
+    /// Waits until at least one byte is available. If an error is encountered,
+    /// returns the error; if the stream ends, returns Ok(0).
+    pub async fn read(&mut self, buf: &mut [u8]) -> Result<usize, error::Error> {
+        // poll_data may return empty chunks, which we need to filter out.
+        // Loop until we get a nonempty chunk, an error, or an end-of-stream.
+        loop {
+            match self.data().await {
+                None => return Ok(0),
+                Some(Err(e)) => return Err(e),
+
+                // Empty chunk; avoid signaling end-of-stream, wait for more data:
+                Some(Ok(bytes)) if bytes.is_empty() => continue,
+
+                Some(Ok(src)) => {
+                    let copy_len = std::cmp::min(buf.len(), src.len());
+                    buf[..copy_len].copy_from_slice(&src[..copy_len]);
+                    let remainder = &src[copy_len..];
+                    if !remainder.is_empty() {
+                        self.chunks.push_front(remainder.into());
+                    }
+                    return Ok(copy_len);
+                }
+            }
+        }
+    }
+
     /// Read the entire body into a byte vector.
     pub async fn read_into_vec(self) -> Result<Vec<u8>, error::Error> {
         let mut body = Box::new(self);
@@ -346,5 +375,152 @@ impl HttpBody for Body {
             }
         }
         SizeHint::with_exact(size)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Read;
+
+    use bytes::{Bytes, BytesMut};
+    use flate2::{Compression, GzBuilder};
+    use http::HeaderMap;
+    use proptest::prelude::*;
+
+    use crate::body::Chunk;
+
+    /// Proptest strategy: get a set of Bytes.
+    fn some_bytes() -> impl Strategy<Value = Bytes> {
+        proptest::collection::vec(any::<u8>(), 0..16).prop_map(|v| v.into())
+    }
+
+    // Gzip some bytes, with "best" compression and no header fields.
+    fn compress_body(body: &[u8]) -> Bytes {
+        let mut encoder =
+            GzBuilder::new().buf_read(std::io::Cursor::new(body), Compression::best());
+        let mut compressed = Vec::new();
+        encoder
+            .read_to_end(&mut compressed)
+            .expect("failed to compress gzip body");
+        compressed.into()
+    }
+
+    // Gradually send the provided body to the provided sender, using the provided chunk lengths.
+    async fn trickle_body(mut sender: hyper::body::Sender, body: Bytes, chunk_lengths: Vec<usize>) {
+        // Put "the whole body" at the back of the chunk-lengths, so we'll send the whole body by
+        // the last iteration.
+        let all_chunks = chunk_lengths.into_iter().chain(std::iter::once(body.len()));
+        let mut remaining: &[u8] = &body;
+        for chunk_length in all_chunks {
+            if remaining.len() == 0 {
+                break;
+            }
+            let len = std::cmp::min(remaining.len(), chunk_length);
+            let to_send = &remaining[..len];
+            remaining = &remaining[len..];
+            let Ok(_) = sender.send_data(Bytes::copy_from_slice(to_send)).await else {
+                return;
+            };
+        }
+        let _ = sender.send_trailers(HeaderMap::default()).await;
+    }
+
+    /// Check that the Body produces the given data, when read using the AsyncRead-like API.
+    async fn check_body_read(
+        mut body: crate::body::Body,
+        want: Bytes,
+    ) -> Result<(), TestCaseError> {
+        let mut got = BytesMut::new();
+        let buf = &mut [0u8; 16];
+        while let Ok(n) = body.read(buf).await {
+            if n == 0 {
+                break;
+            }
+            got.extend_from_slice(&buf[..n]);
+        }
+        prop_assert_eq!(got.len(), want.len());
+        for (i, (got, want)) in got.into_iter().zip(want.into_iter()).enumerate() {
+            prop_assert_eq!(got, want, "@{}: {} != {}", i, got, want);
+        }
+
+        Ok(())
+    }
+
+    /// Test that a given body can round-trip, even if the body is split into chunks.
+    async fn test_chunked_body(
+        bytes: Bytes,
+        chunk_lengths: Vec<usize>,
+    ) -> Result<(), TestCaseError> {
+        let mut js = tokio::task::JoinSet::default();
+        let (sender, body) = hyper::Body::channel();
+        js.spawn({
+            let bytes = bytes.clone();
+            async move {
+                trickle_body(sender, bytes, chunk_lengths).await;
+                Ok(())
+            }
+        });
+        js.spawn(check_body_read(body.into(), bytes));
+        let results = js.join_all().await;
+        for result in results {
+            result?;
+        }
+        Ok(())
+    }
+
+    /// Test that a given body can round-trip the Gzip decoder, even if the compressed body
+    /// is split into chunks.
+    async fn test_gzip_body(
+        ungz_bytes: Bytes,
+        chunk_lengths: Vec<usize>,
+    ) -> Result<(), TestCaseError> {
+        let gz_bytes = compress_body(&ungz_bytes);
+
+        let mut js = tokio::task::JoinSet::default();
+        let (sender, gz_body) = hyper::Body::channel();
+        js.spawn({
+            let gz_bytes = gz_bytes.clone();
+            async move {
+                trickle_body(sender, gz_bytes, chunk_lengths).await;
+                Ok(())
+            }
+        });
+        let ungz_body: crate::body::Body = Chunk::compressed_body(gz_body).into();
+        js.spawn(check_body_read(ungz_body, ungz_bytes));
+        let results = js.join_all().await;
+        for result in results {
+            result?;
+        }
+        Ok(())
+    }
+
+    proptest! {
+        #[test]
+        fn gzip_chunks_reproduce_body(
+            (body, chunk_lengths) in some_bytes().prop_flat_map(|bytes| {
+                    let len = bytes.len();
+                    let chunk_length_strategy= proptest::collection::vec(0..=len, 0..=(len/8));
+            (Just(bytes), chunk_length_strategy)
+            }),
+        ) {
+            let rt = tokio::runtime::Builder::new_current_thread().build().unwrap();
+            return rt.block_on(test_gzip_body(body, chunk_lengths));
+        }
+
+    }
+
+    proptest! {
+        #[test]
+        fn chunks_reproduce_body(
+            (body, chunk_lengths) in some_bytes().prop_flat_map(|bytes| {
+                    let len = bytes.len();
+                    let chunk_length_strategy= proptest::collection::vec(0..=len, 0..=(len/8));
+            (Just(bytes), chunk_length_strategy)
+            }),
+        ) {
+            let rt = tokio::runtime::Builder::new_current_thread().build().unwrap();
+            return rt.block_on(test_chunked_body(body, chunk_lengths));
+        }
+
     }
 }
