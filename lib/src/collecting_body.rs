05464b390e26c4411d6cd3e86705ad3e8a3926fa
Charles Eckman <charles.eckman@fastly.com>
2025-06-27 15:51:43 -0400

    Range-request support in CollectingBody
    Allow `read_range` in CollectingBody with a start (and optional end)
    offset. Add helpers to allow waiting for a known or minimum length,
    which we need to get a consistent blocking behavior with Compute.
    
    Add the proptest regressions files for body and collecting_body.

--- lib/src/collecting_body.rs
+++ lib/src/collecting_body.rs
@@ -50,6 +50,57 @@ impl CollectingBody {
         }
     }
 
+    /// Wait until the length is known or an error is produced.
+    pub async fn known_length(&self) -> Result<u64, error::Error> {
+        let mut recv = self.inner.clone();
+        let state = recv
+            .wait_for(|state| !matches!(state, CollectingBodyInner::Streaming(_)))
+            .await
+            .expect("CollectingBody terminated, but not in state Complete or Error");
+        match &*state {
+            CollectingBodyInner::Streaming(_) => unreachable!("wait_for un-matched this"),
+            CollectingBodyInner::Complete { body, .. } => {
+                Ok(body.iter().map(|v| v.len() as u64).sum())
+            }
+            CollectingBodyInner::Error(error) => {
+                tracing::warn!(
+                    "could not determine length of cache body; write error: {}",
+                    error
+                );
+                Err(Error::UnfinishedStreamingBody)
+            }
+        }
+    }
+
+    /// Wait until at least this many bytes have been written, or the body is complete.
+    /// Returns Ok() if the body has at least `want` bytes.
+    pub async fn wait_length(&self, want: u64) -> Result<(), error::Error> {
+        let mut recv = self.inner.clone();
+        let state = recv
+            .wait_for(|state| match state {
+                CollectingBodyInner::Streaming(items) => {
+                    let length: u64 = items.iter().map(|v| v.len() as u64).sum();
+                    length >= want
+                }
+                _ => true,
+            })
+            .await
+            .expect("CollectingBody terminated, but not in state Complete or Error");
+
+        match &*state {
+            CollectingBodyInner::Error(_) => Err(Error::UnfinishedStreamingBody),
+            CollectingBodyInner::Complete { body, .. } => {
+                let length: u64 = body.iter().map(|v| v.len() as u64).sum();
+                if length >= want {
+                    Ok(())
+                } else {
+                    Err(Error::UnfinishedStreamingBody)
+                }
+            }
+            CollectingBodyInner::Streaming(_) => Ok(()),
+        }
+    }
+
     /// Create a new CollectingBody that stores & streams from the provided Body.
     ///
     /// Writes to the StreamingBody are collected, and propagated to all readers of this
@@ -139,10 +190,17 @@ impl CollectingBody {
 
     /// Get a new read handle to this body.
     pub fn read(&self) -> Result<Body, error::Error> {
+        self.read_range(0, None)
+    }
+
+    /// Get a handle to read the requested range from this body.
+    /// Start is inclusive, end is exclusive.
+    pub fn read_range(&self, start: u64, end: Option<u64>) -> Result<Body, error::Error> {
         let mut upstream = self.inner.clone();
         let (mut tx, rx) = StreamingBody::new();
         tokio::task::spawn(async move {
             let mut next_chunk = 0;
+            let mut cursor = 0u64;
             // The receiver tracks the "current" value, and assumes that the value at the receiver
             // is "seen" to begin with.
             // So we have a do-while loop, with the "changed" condition at the bottom.
@@ -168,18 +226,57 @@ impl CollectingBody {
                 };
 
                 // If send_chunks is nonempty, it contains data for us to forward:
+                next_chunk += send_chunks.len();
                 for chunk in send_chunks {
+                    let chunk_start = cursor;
+                    let chunk_end = cursor + chunk.len() as u64;
+                    cursor = chunk_end;
+
+                    if end.is_some_and(|end| chunk_start >= end) {
+                        // We have sent all the bytes we need to.
+                        let _ = tx.finish();
+                        return;
+                    }
+
+                    // We need to send either the whole chunk, or a portion of it.
+                    let slice_start = std::cmp::max(start, chunk_start);
+                    let slice_end = std::cmp::min(end.unwrap_or(u64::MAX), chunk_end);
+                    if slice_end <= slice_start {
+                        // Empty slice, skip this chunk.
+                        continue;
+                    }
+
+                    let chunk = if slice_start == chunk_start && slice_end == chunk_end {
+                        // Proceed without copy
+                        chunk
+                    } else {
+                        // Copy out only the bytes of interest
+                        let range = slice_start.saturating_sub(chunk_start) as usize
+                            ..(slice_end.saturating_sub(chunk_start)) as usize;
+                        Bytes::copy_from_slice(&chunk[range])
+                    };
+
                     if tx.send_chunk(chunk).await.is_err() {
                         // Reader hung up; we don't care any more.
                         return;
                     }
-                    next_chunk += 1;
                 }
                 // And we may have gotten the trailers, which are the "body is done" signal:
                 if let Some(trailers) = trailers {
                     for (k, v) in trailers.iter() {
                         tx.append_trailer(k.clone(), v.clone());
                     }
+
+                    // Did the body terminate before our "end" offset?
+                    if let Some(end) = end {
+                        if cursor < end {
+                            // Reached the end-of-input without getting the offset we wanted.
+                            // This should be an "unfinished streaming body" error;
+                            // so, return without "finish"ing.
+                            return;
+                        }
+                    }
+
                     // We don't wait for the channel to be closed;
                     // if the object stays in the cache, the object will be around ~forever.
                     // Trailers sent -> we're done.
@@ -246,6 +343,7 @@ impl CollectingBodyInner {
 mod tests {
     use std::sync::Arc;
 
+    use bytes::{Bytes, BytesMut};
     use http::{HeaderName, HeaderValue};
     use tokio::{sync::oneshot, task::JoinSet};
 
@@ -255,6 +353,7 @@ mod tests {
         streaming_body::StreamingBody,
         Error,
     };
+    use proptest::prelude::*;
 
     #[tokio::test]
     async fn stream_and_collect() {
@@ -454,7 +553,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn error_on_underfill() {
+    async fn error_on_expected_length_underfill() {
         let (mut tx, rx) = StreamingBody::new();
 
         let collect = CollectingBody::new(Body::from(Chunk::from(rx)), Some(10));
@@ -463,6 +562,21 @@ mod tests {
         tx.finish().unwrap();
 
         let err = collect.read().unwrap().read_into_vec().await.unwrap_err();
+        assert!(matches!(err, Error::UnfinishedStreamingBody));
+    }
+
+    #[tokio::test]
+    async fn error_on_range_underfill() {
+        let (mut tx, rx) = StreamingBody::new();
+
+        let collect = CollectingBody::new(Body::from(Chunk::from(rx)), None);
+
+        let reader = collect.read_range(0, Some(10)).unwrap();
+
+        tx.send_chunk(b"hello".as_slice()).await.unwrap();
+        tx.finish().unwrap();
+
+        let err = reader.read_into_vec().await.unwrap_err();
         assert!(matches!(err, Error::UnfinishedStreamingBody));
     }
 
@@ -477,5 +591,149 @@ mod tests {
 
         let err = collect.read().unwrap().read_into_vec().await.unwrap_err();
         assert!(matches!(err, Error::UnfinishedStreamingBody));
+    }
+
+    /// Proptest strategy: get a nonempty set of Bytes.
+    fn some_bytes() -> impl Strategy<Value = Bytes> {
+        proptest::collection::vec(any::<u8>(), 1..16).prop_map(|v| v.into())
+    }
+
+    /// Proptest strategy: a nonempty set of chunks (Byte blobs).
+    fn some_chunks() -> impl Strategy<Value = Vec<Bytes>> {
+        proptest::collection::vec(some_bytes(), 1..16)
+    }
+
+    /// Utility function for converting from other errors to a TestCaseError, with an annotation.
+    fn fail_test_case<E: std::fmt::Display>(
+        note: &str,
+    ) -> impl Fn(E) -> TestCaseError + use<'_, E> {
+        move |err: E| TestCaseError::fail(format!("{note}: {}", err))
+    }
+
+    async fn test_start_range(body_chunks: Vec<Bytes>, start: u64) -> Result<(), TestCaseError> {
+        let full_body: Bytes = body_chunks
+            .iter()
+            .cloned()
+            .fold(BytesMut::new(), |mut acc, b| {
+                acc.extend(b);
+                acc
+            })
+            .into();
+        if start as usize >= full_body.len() {
+            return Err(TestCaseError::Reject("invalid start".into()));
+        }
+
+        let (mut tx, rx) = StreamingBody::new();
+        let cb = CollectingBody::new(rx.into(), None);
+
+        let mut js = JoinSet::new();
+        js.spawn(async move {
+            for chunk in body_chunks {
+                tx.send_chunk(chunk)
+                    .await
+                    .map_err(fail_test_case("error sending chunk"))?;
+            }
+            tx.finish()
+                .map_err(fail_test_case("error finishing write"))?;
+            Ok(())
+        });
+        js.spawn(async move {
+            let body = cb
+                .read_range(start, None)
+                .map_err(fail_test_case("error getting body"))?;
+            let got = body
+                .read_into_vec()
+                .await
+                .map_err(fail_test_case("error reading body"))?;
+            let want = &full_body[(start as usize)..];
+
+            prop_assert_eq!(&got, want, "mismatched body data");
+
+            Ok(())
+        });
+
+        let _: Vec<_> = js.join_all().await.into_iter().collect::<Result<_, _>>()?;
+        Ok(())
+    }
+
+    async fn test_start_end_range(
+        body_chunks: Vec<Bytes>,
+        start: u64,
+        end: u64,
+    ) -> Result<(), TestCaseError> {
+        let full_body: Bytes = body_chunks
+            .iter()
+            .cloned()
+            .fold(BytesMut::new(), |mut acc, b| {
+                acc.extend(b);
+                acc
+            })
+            .into();
+        if start as usize >= full_body.len() {
+            return Err(TestCaseError::Reject("invalid start".into()));
+        }
+
+        let (mut tx, rx) = StreamingBody::new();
+        let cb = CollectingBody::new(rx.into(), None);
+
+        let mut js = JoinSet::new();
+        js.spawn(async move {
+            for chunk in body_chunks {
+                tx.send_chunk(chunk)
+                    .await
+                    .map_err(fail_test_case("error sending chunk"))?;
+            }
+            tx.finish()
+                .map_err(fail_test_case("error finishing write"))?;
+            Ok(())
+        });
+        js.spawn(async move {
+            let body = cb
+                .read_range(start, Some(end))
+                .map_err(fail_test_case("error getting body"))?;
+            let got = body
+                .read_into_vec()
+                .await
+                .map_err(fail_test_case("error reading body"))?;
+            let want = &full_body[(start as usize)..(end as usize)];
+
+            prop_assert_eq!(&got, want, "mismatched body data");
+
+            Ok(())
+        });
+
+        let _: Vec<_> = js.join_all().await.into_iter().collect::<Result<_, _>>()?;
+        Ok(())
+    }
+
+    proptest! {
+        #[test]
+        fn read_body_from_start(
+            (body, start) in some_chunks().prop_flat_map(|chunks| {
+                    let len = chunks.iter().map(|v| v.len() as u64).sum();
+                    (Just(chunks), 0..len)
+            }),
+        ) {
+            let rt = tokio::runtime::Builder::new_current_thread().build().unwrap();
+            rt.block_on(test_start_range(body, start))?;
+        }
+
+    }
+
+    proptest! {
+        #[test]
+        fn read_body_start_end(
+            (body, start, end) in some_chunks().prop_flat_map(|chunks| {
+                    let len = chunks.iter().map(|v| v.len() as u64).sum();
+                    (Just(chunks), 0..len, Just(len))
+            }).prop_flat_map(|(chunks, start, len)| {
+                // Inclusive of start is fine:
+                let end = start..len;
+            (Just(chunks), Just(start), end)
+        })) {
+            let rt = tokio::runtime::Builder::new_current_thread().build().unwrap();
+            rt.block_on(test_start_end_range(body, start, end))?;
+        }
+
     }
 }
